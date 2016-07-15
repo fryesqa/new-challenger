274ff748ed8743d7c4b2508b716de152c2d84ba3
Frances Yang <franyang99@gamil.com>
2016-07-15 13:43:38 -0700

    Fix typos

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../client/public'));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
-app.listen(port, funtion() {
+app.listen(port, function() {
   console.log('Listening on port', port);
 });
