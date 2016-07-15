5c13a3af4c7ab8202c0c149f7233f69d0c3a3c19
Carl Chen <carlchen214@gmail.com>
2016-07-15 16:29:06 -0700

    Feat/ setup Login file structure and image

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var Login = (props) => (
+  <div> 
+    <div className="login"> <img src="./images/facebook-login-button.png" onClick=""/> </div>
+  </div>
+  );
+
+Login.propTypes = {
+};
+
+export default Login;
