8806c0984047977cff93d7adf04ef00f93a72938
Carl Chen <carlchen214@gmail.com>
2016-07-15 15:36:00 -0700

    Feat/ Add dummy data to App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import Router from 'react-router';
-// import ChallengeList from './ChallengeList.jsx'
+import ChallengeList from './ChallengeList.jsx'
+
+const data = [{
+        id: '1',
+        username: 'Bob Belcher',
+        text: 'Eat 5 hamburgers',
+        image: "https://upload.wikimedia.org/wikipedia/en/7/7f/Bob's_Burgers_promo.png",
+        date: new Date()
+      }, {
+        id: '2',
+        username: 'Bobby Belchy',
+        text: 'Eat 10 hamburgers',
+        image: "https://upload.wikimedia.org/wikipedia/en/7/7f/Bob's_Burgers_promo.png",
+        date: new Date()
+      }];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      challenges: [{
-        username: "Bob Belcher",
-        text: "Eat 5 hamburgers",
-        image: "https://upload.wikimedia.org/wikipedia/en/7/7f/Bob's_Burgers_promo.png",
-        date: new Date()
-      }]
+      challenges: data
     };
   }
 
@@ -19,6 +28,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>New Challenger</h1> 
+        <ChallengeList challenges={this.state.challenges} />
       </div>
     )
   }
