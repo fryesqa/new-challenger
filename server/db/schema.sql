5f2e5cb18ce12c921fcd2e48aacb0aec3188ef86
Frances Yang <franyang99@gamil.com>
2016-07-15 18:55:55 -0700

    Add columns to tables to track dates

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,12 +3,13 @@ CREATE TABLE IF NOT EXISTS user (
   name VARCHAR(100),
   email VARCHAR(100),
   url TEXT,
-  facebookId TEXT 
+  facebookId TEXT,
+  createdAt TIMESTAMP WITH TIME ZONE DEFAULT now() 
 );
 
-CREATE TABLE IF NOT EXISTS type (
+CREATE TABLE IF NOT EXISTS category (
   id SERIAL PRIMARY KEY,
-  genre VARCHAR(50)
+  name VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS challenge (
@@ -16,8 +17,12 @@ CREATE TABLE IF NOT EXISTS challenge (
   name VARCHAR(100),
   description TEXT,
   url TEXT,
+  challengers INTEGER,
+  successes INTEGER,
   creatorId INTEGER,
-  typeId INTEGER,
+  categoryId INTEGER,
+  createdAt TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  endTime TIMESTAMP WITH TIME ZONE,
   CONSTRAINT challenge_creator_fk FOREIGN KEY creatorId REFERENCES user (id),
   CONSTRAINT challenge_type_fk FOREIGN KEY typeId REFERENCES type (id) 
 )
@@ -25,7 +30,9 @@ CREATE TABLE IF NOT EXISTS challenge (
 CREATE TABLE IF NOT EXISTS user_challenge (
   id SERIAL PRIMARY KEY,
   userId INTEGER,
-  challengeId INTEGER
+  challengeId INTEGER,
+  timeAccepted TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  timeCompleted TIMESTAMP WITH TIME ZONE, 
   CONSTRAINT join_user_fk FOREIGN KEY userId REFERENCES user (id),
   CONSTRAINT join_challenge_fk FOREIGN KEY challengeId REFERENCES challenge (id) 
 );
