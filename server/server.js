67fa41fae63219925ca41159e0e83e8b3178940a
Frances Yang <franyang99@gamil.com>
2016-07-17 12:24:49 -0700

    Setup server-side rendering of pages

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import { renderToString } from 'react/dom'
+import { match, RouterContext } from 'react-router'
+import routes from '../client/src/routes'
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -17,9 +22,28 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.use(passport.initialize());
 
+// need to use middleware to do facebook auth step
+// ****** To Fix ******
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 app.get('/auth/facebook/callback', 
   passport.authenticate('facebook', { successRedirect: '/#/main', failureRedirect: '/'}));
+
+
+app.get('*', function(req, res) {
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else if (redirect) {
+      res.redirect(redirect.pathname + redirect.search);
+    } else if (props) {
+      const appHtml = renderToString(<RouterContext {...props} />);
+      res.send(renderPage(appHtml)); 
+    } else {
+      res.status(404).send('Not Found');
+    } 
+  });
+});
+
 
 app.listen(port, function() {
   console.log('Listening on port', port);
