dec77afc50e3f6a80acc2309ea436d422b908540
Keith Wong <keith.wong3672@gmail.com>
2016-07-17 11:16:57 -0700

    Add required modules and routes for facebook

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,6 +11,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../client/public')));
+
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/profile', failureRedirect: '/home'}));
 
 app.listen(port, function() {
   console.log('Listening on port', port);
