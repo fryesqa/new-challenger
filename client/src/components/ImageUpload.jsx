b5d796e5e88d3b797b963216b86b618758e47c6c
Carl Chen <carlchen214@gmail.com>
2016-07-17 14:11:55 -0700

    feat/ add 'es6-promise' as polyfill and isomorphic-fetch as dependencies

--- client/src/components/ImageUpload.jsx
+++ client/src/components/ImageUpload.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+var Promise = require ('es6-promise').polyfill();
+var fetch = require ('isomorphic-fetch');
 
 class ImageUpload extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  // fetch('http://www.google.com')
+  //     .then(function(response) {
+  //         if (response.status >= 400) {
+  //             throw new Error("Bad response from server");
+  //         }
+  //         console.log(response.json());
+  //     })
+  //     .then(function(stories) {
+  //         console.log(stories);
+  //     });
+
   render() {
     return (
       <div> 
-      Image Upload
+        Image Upload
+        <input type="file" />
+        <input type="submit" />
       </div>
       );
   }
