9d616e4cd8b918a11967da507ca4288879967f72
Carl Chen <carlchen214@gmail.com>
2016-07-18 10:40:14 -0700

    fix / fix challengelistentry & dummydata to fit updated schema fields

--- client/src/components/ChallengeListEntry.jsx
+++ client/src/components/ChallengeListEntry.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 // Needs more styling depending on fields
 var ChallengeListEntry = ({challenge}) => (
   <div className="challenge"> 
-  <div className="image"> <img src={challenge.image} /> </div>
-  <div className="username"> Challenger: {challenge.username} </div>
-  <div className="text"> The Challenge: {challenge.text} </div>
+  <div className="image"> <img src={challenge.imageUrl} /> </div>
+  <div className="name"> Challenge Name: {challenge.name} </div>
+  <div className="username"> Challenge Creator: {challenge.username} </div>
+  <div className="description"> The Challenge: {challenge.description} </div>
+  <div className="category"> Challenge Category: {challenge.category} </div>
+  <div className="number"> Number of challengers: {challenge.challengers} </div>
+  <div className="successes"> Successfully completed: {challenge.successes} </div>
   <div className="createdAt">Challenge was issued on: {challenge.createdAt} </div>
+  <div className="endTime">Challenge ends on {challenge.endTime} </div>
   </div>
 );
 
--- client/src/components/dummyData.js
+++ client/src/components/dummyData.js
@@ -2,16 +2,27 @@
 
 var dummyData = [{
         id: '1',
-        username: 'Bob Belcher',
-        text: 'Eat 5 hamburgers',
-        image: "https://upload.wikimedia.org/wikipedia/en/7/7f/Bob's_Burgers_promo.png",
-        createdAt: new Date().toString()
+        imageUrl: "https://upload.wikimedia.org/wikipedia/en/7/7f/Bob's_Burgers_promo.png",
+        name: 'Burger time!',
+        username: 'Bob Belcher', //creatorId?
+        description: 'Eat 5 hamburgers',
+        category: 'Food',
+        challengers: 10,
+        successes: 8,
+        createdAt: new Date().toString(),
+        endTime: new Date().toString()
       }, {
         id: '2',
+        imageUrl: "http://i.onionstatic.com/avclub/5748/94/original/304.jpg",
+        name: 'Burger frenzy!',
         username: 'Bobby Belchy',
-        text: 'Eat 10 hamburgers',
-        image: "http://i.onionstatic.com/avclub/5748/94/original/304.jpg",
-        createdAt: new Date().toString()
+        description: 'Eat 10 hamburgers',
+        category: 'Food',
+        challengers: 20,
+        successes: 3,
+        createdAt: new Date().toString(),
+        endTime: new Date().toString()
       }];
 
 export default dummyData; 
+
