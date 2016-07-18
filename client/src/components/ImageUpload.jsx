c6d1198b8e75263cdcef6703756e85de916aa524
Carl Chen <carlchen214@gmail.com>
2016-07-17 22:51:40 -0700

    feat/ set up react-dropzone for imageupload, add it as dependency

--- client/src/components/ImageUpload.jsx
+++ client/src/components/ImageUpload.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Dropzone from 'react-dropzone'
 var Promise = require ('es6-promise').polyfill();
 var fetch = require ('isomorphic-fetch');
 
@@ -7,7 +8,11 @@ class ImageUpload extends React.Component {
     super(props);
   }
 
-  // fetch('http://www.google.com')
+  onDrop(files) {
+    console.log('Received files:', files);
+  }
+
+  // fetch('localhost:3000/upload')
   //     .then(function(response) {
   //         if (response.status >= 400) {
   //             throw new Error("Bad response from server");
@@ -18,12 +23,16 @@ class ImageUpload extends React.Component {
   //         console.log(stories);
   //     });
 
+  // or if just want to use html
+  // <input type="file" />
+  // <input type="submit" />
   render() {
     return (
       <div> 
         Image Upload
-        <input type="file" />
-        <input type="submit" />
+        <Dropzone onDrop={this.onDrop} multiple={false}>
+          <div>Drop an image here, or click to select an image to upload.</div>
+        </Dropzone>
       </div>
       );
   }
