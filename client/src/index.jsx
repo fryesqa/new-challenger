578a7f25834fbfa5f86160c19f2bb7123ae7f57d
Frances Yang <franyang99@gamil.com>
2016-07-18 12:43:23 -0700

    Add react router to index page, currently using hashHistory

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import React from 'react'
+import { Router, Route, hashHistory } from 'react-router'
+import ReactDOM from 'react-dom'
+import App from './components/App.jsx'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+  </Router>
+), document.getElementById('app'))
