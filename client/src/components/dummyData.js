def2ddc6a3ba8d5fbd0a527c5e8ebcb1e32a2463
Frances Yang <franyang99@gamil.com>
2016-07-18 14:28:21 -0700

    Add dummy data for user profile

--- client/src/components/dummyData.js
+++ client/src/components/dummyData.js
@@ -1,28 +1,39 @@
 // dummy data for populating challenge components
 
-var dummyData = [{
-        id: '1',
-        imageUrl: "https://upload.wikimedia.org/wikipedia/en/7/7f/Bob's_Burgers_promo.png",
-        name: 'Burger time!',
-        username: 'Bob Belcher', //creatorId?
-        description: 'Eat 5 hamburgers',
-        category: 'Food',
-        challengers: 10,
-        successes: 8,
-        createdAt: new Date().toString(),
-        endTime: new Date().toString()
-      }, {
-        id: '2',
-        imageUrl: "http://i.onionstatic.com/avclub/5748/94/original/304.jpg",
-        name: 'Burger frenzy!',
-        username: 'Bobby Belchy',
-        description: 'Eat 10 hamburgers',
-        category: 'Food',
-        challengers: 20,
-        successes: 3,
-        createdAt: new Date().toString(),
-        endTime: new Date().toString()
-      }];
+var dummyData = {
+  challenges: [
+    {
+      id: '1',
+      imageUrl: "https://upload.wikimedia.org/wikipedia/en/7/7f/Bob's_Burgers_promo.png",
+      name: 'Burger time!',
+      username: 'Bob Belcher', //creatorId?
+      description: 'Eat 5 hamburgers',
+      category: 'Food',
+      challengers: 10,
+      successes: 8,
+      createdAt: new Date().toString(),
+      endTime: new Date().toString()
+    },
+    {
+      id: '2',
+      imageUrl: "http://i.onionstatic.com/avclub/5748/94/original/304.jpg",
+      name: 'Burger frenzy!',
+      username: 'Bobby Belchy',
+      description: 'Eat 10 hamburgers',
+      category: 'Food',
+      challengers: 20,
+      successes: 3,
+      createdAt: new Date().toString(),
+      endTime: new Date().toString()
+    }
+  ],
+  user: {
+    id: '1',
+    username: 'Jimmy Pesto',
+    imageUrl: 'http://vignette3.wikia.nocookie.net/bobsburgerpedia/images/3/32/Jimmy_Pesto.png/revision/latest?cb=20130305162049',
+    email: 'jimmypesto@bobsucks.com'
+  }
+};
 
 export default dummyData; 
 
