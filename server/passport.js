ea080114d80c661b93870fc306f7f4bb54007870
Keith Wong <keith.wong3672@gmail.com>
2016-07-17 19:33:08 -0700

    Add passport dependencies and serialize user

---
+++ server/passport.js
@@ -0,0 +1,17 @@
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+var config = require('./config/auth-facebook.js');
+
+passport.serializeUser(function(user, done) {
+  // placeholder for custom user serialization
+  // null is for errors
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  // placeholder for custom user deserialization.
+  // maybe you are going to get the user from mongo by id?
+  // null is for errors
+  done(null, user);
+});
