ac777be6873c3c40fd0909bc55ced8bde2130111
Keith Wong <keith.wong3672@gmail.com>
2016-07-17 20:57:40 -0700

    Add link to authentication user

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // just some basic file structure, feel free to change
 var Login = (props) => (
   <div> 
-    <div className="login"> <img src="./images/facebook-login-button.png" onClick=""/> </div>
+    <div className="login"> <a href="/auth/facebook"><img src="./images/facebook-login-button.png" onClick=""/></a> </div>
   </div>
   );
 
