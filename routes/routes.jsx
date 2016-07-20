2585f22aee29f3462ce81d63190545b6ed338c1f
1kindjune <ejw322@nyu.edu>
2016-07-20 16:29:42 -0700

    Change ChallengeInfoUser to PlayerList

--- routes/routes.jsx
+++ routes/routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import { App, Main, Login, AdminChallenge, ChallengeCreateForm, UserProfile, ChallengeInfoUser, SubmitAttempt } from '../client/src/components/Components.js';
+import { App, Main, Login, AdminChallenge, ChallengeCreateForm, UserProfile, PlayerList, SubmitAttempt } from '../client/src/components/Components.js';
 
 module.exports = (
   <div>
     <Route path="/" component={App}>
       <IndexRoute component={Main} />
       <Route path="/challenge/create" component={ChallengeCreateForm} />
-      <Route path="/challenge/:challengeId" component={ChallengeInfoUser} />
+      <Route path="/challenge/:challengeId" component={PlayerList} />
       <Route path="/challenge/:challengeId/admin" component={AdminChallenge} />
       <Route path="/challenge/:challengeId/submission" component={SubmitAttempt} />
     </Route>
