0374edd8f262eeaa3f209fa93cf49cd2d7771da7
Keith Wong <keith.wong3672@gmail.com>
2016-07-20 15:31:45 -0700

    Clean up code

--- server/db/sequelize.js
+++ server/db/sequelize.js
@@ -5,7 +5,6 @@ var pgHstore = require('pg-hstore');
 // currently the shell script creates database with user as $USER
 // which creates a username based on your computer user name will need
 // to change
-
 var sequelize = new Sequelize('newchallenger', 'kwong', '', {
   dialect: 'postgres',
   host: 'localhost',
@@ -14,7 +13,8 @@ var sequelize = new Sequelize('newchallenger', 'kwong', '', {
   }
 });
 
-// User model
+
+// user model
 exports.User = sequelize.define('user', {
   name: Sequelize.STRING(100),
   email: Sequelize.STRING(100),
