f40dc7951e9d17a13d2df34fcd6db7a8e02f4ba9
Frances Yang <franyang99@gamil.com>
2016-07-19 21:57:07 -0700

    Configure webpack for server side bundling

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,14 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  entry: './server/server.js',
+  entry: path.resolve(__dirname, 'server', 'server.js'),
+  target: 'node',
+  externals: fs.readdirSync(path.resolve(__dirname, 'node_modules')).concat([
+    'react-dom/server', 'react/addons',
+  ]).reduce(function (ext, mod) {
+    ext[mod] = 'commonjs ' + mod
+    return ext
+  }, {}),
   output: {
     path: './server',
     filename: 'server.bundle.js'
+  },
+  node: {
+    __filename: true,
+    __dirname: true
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
-        include: [/server/, /routes/]
+        exclude: /node_modules/,
         loader: 'babel',
       },
     ],
