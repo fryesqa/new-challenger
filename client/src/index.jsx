567113beef5879ad321ea3eacf18541dea9d4dcc
Frances Yang <franyang99@gamil.com>
2016-07-19 21:49:51 -0700

    Fix import paths

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import ReactDOM from 'react-dom';
-import routes from '../../routes/routes.js';
+import routes from '../../routes/routes.jsx';
 
 ReactDOM.render(
   <Router history={browserHistory} routes={routes} />,
