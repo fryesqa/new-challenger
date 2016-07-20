0d637f2bcfdc6e906eac065e75745b81f5bf803c
Frances Yang <franyang99@gamil.com>
2016-07-19 17:13:21 -0700

    Link to detail challenge page

--- client/src/components/ChallengeList.jsx
+++ client/src/components/ChallengeList.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
 import ChallengeListEntry from './ChallengeListEntry.jsx';
 
 // takes in props.challenges, and maps over to create ChallengeListEntry
-var ChallengeList = ({challenges}) => (
+const ChallengeList = ({challenges, currentUser}) => (
   <div> 
    {challenges.map(function(challenge, i) {
     return (
-      <div key={challenge.id}> 
-        <h3> Challenge {i + 1}</h3> 
-        <ChallengeListEntry challenge={challenge} />
+      <div key={challenge.id}>
+        <Link to={currentUser === challenge.creatorId ? `/challenge/${challenge.id}/admin` : `/challenge/${challenge.id}`}>
+          <h3> Challenge {i + 1}</h3> 
+          <ChallengeListEntry challenge={challenge} />
+        </Link>
       </div>
     );
    })}
