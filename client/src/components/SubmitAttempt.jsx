c0529126b85c82f4183f8a2a7082751b89181b2e
Keith Wong <keith.wong3672@gmail.com>
2016-07-21 09:11:41 -0700

    Create challenge submit page

--- client/src/components/SubmitAttempt.jsx
+++ client/src/components/SubmitAttempt.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 
-var SubmitAttempt = (props) => (
-  <h1>In Progress</h1>
-)
+var SubmitAttempt = React.createClass({
+  getInitialState: function() {
+      return {
+        files: []
+      };
+  },
+
+  onDrop: function (files) {
+    this.setState({
+      files: files
+    });
+  },
+
+  onOpenClick: function() {
+    this.refs.dropzone.open()
+  },
+
+  submit: function() {
+    console.log('uploading files')
+    // ajax call goes here
+  },
+
+  render: function () {
+    return (
+      <div>
+        <Dropzone ref="dropzone" onDrop={this.onDrop}>
+            <div>Drag your video or image proving succussful challenge here.</div>
+        </Dropzone>
+        <div>
+          <button onClick={this.onOpenClick}> Attach File </button>
+          <button onClick={this.submit}>Submit Proof</button>
+        </div>
+        {this.state.files.length > 0 ? <div>
+        <h2>Uploading {this.state.files.length} files...</h2>
+        <div>{this.state.files.map((file) => <img src={file.preview} /> )}</div>
+        </div> : null}
+      </div>
+    );
+  }
+});
 
 export default SubmitAttempt;
