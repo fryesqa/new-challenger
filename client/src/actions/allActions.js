d1c6883672607498fe20e3b4f6331547340b8c5e
Frances Yang <franyang99@gamil.com>
2016-07-22 11:55:56 -0700

    Fix importing of fetch challenges

--- client/src/actions/allActions.js
+++ client/src/actions/allActions.js
@@ -1,7 +1,7 @@
 //All Actions
 //Import actions here
 import createChallenge from './createChallenge';
-import fetchChallenges from './fetchChallenges';
+import { fetchChallenges } from './fetchChallenges';
 
 //test function
 export function increment(index) {
