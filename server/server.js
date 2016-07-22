537290ca1c336d2d38c1473f60f3813ffd1d6cd1
1kindjune <ejw322@nyu.edu>
2016-07-22 16:46:24 -0700

    Remove webpack for server and simplify serverside handling of react routing

--- server/server.js
+++ server/server.js
@@ -1,8 +1,3 @@
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { match, RouterContext } from 'react-router'
-import routes from '../routes/routes.jsx'
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -11,9 +6,6 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const pgClient = require('./db/connection.js');
 const passportFacebook = require('./passport.js');
 const session = require('express-session');
-
-// dummyData
-import dummyData from '../client/src/components/dummyData.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -69,55 +61,11 @@ app.get('/challenges', function(req, res) {
 
 //https://github.com/reactjs/react-router-tutorial/tree/master/lessons/13-server-rendering
 app.get('*', function(req, res) {
-  match({ routes: routes, location: req.url }, (err, redirect, props) => {
-    if (err) {
-      res.status(500).send(err.message);
-    } else if (redirect) {
-      res.redirect(redirect.pathname + redirect.search);
-    } else if (props) {
-      const appHtml = renderToString(<RouterContext {...props} />);
-      res.send(renderPage(appHtml)); 
-    } else {
-      res.status(404).send('Not Found');
-    } 
-  });
+  res.sendFile(path.join(__dirname, '/../client/public/index.html'));
 });
 
 
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
-
-/////////////////////////////////////
-function renderPage(appHtml) {
-  return ` 
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Challenger</title>
-
-        <!-- Materialize -->
-        <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-
-        <!-- Custom Styling -->
-        <link rel="stylesheet" href="/styles/styles.css">
-        <link rel="stylesheet" href="https://npmcdn.com/react-select/dist/react-select.css">
-        
-      </head>
-      <body>
-        <div id='app'>${appHtml}</div>
-        <!-- bundle.js contains all of webpack's compiled & transpiled JSX -->
-        <script src="./bundle.js"></script>
-
-        <!-- Scripts -->
-
-        <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-      </body>
-    </html>
-  ` 
-}
 
