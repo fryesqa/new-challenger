539fdec344bc330c10d307911aa54cca11444aca
Frances Yang <franyang99@gamil.com>
2016-07-22 10:51:34 -0700

    Add fetchChallenges to all actions

--- client/src/actions/allActions.js
+++ client/src/actions/allActions.js
@@ -1,7 +1,7 @@
 //All Actions
 //Import actions here
 import createChallenge from './createChallenge';
-
+import fetchChallenges from './fetchChallenges';
 
 //test function
 export function increment(index) {
@@ -12,4 +12,4 @@ export function increment(index) {
   };
 }
 
-export { createChallenge };
+export { createChallenge, fetchChallenges };
