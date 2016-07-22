45bced7e00ea6ddcf08792aadc60620a0d26f1b2
Frances Yang <franyang99@gamil.com>
2016-07-22 09:07:32 -0700

    Lookup challenges in entities using ids

--- client/src/components/ChallengeList.jsx
+++ client/src/components/ChallengeList.jsx
@@ -3,22 +3,26 @@ import { Link } from 'react-router';
 import ChallengeListEntry from './ChallengeListEntry.jsx';
 
 // takes in props.challenges, and maps over to create ChallengeListEntry
-const ChallengeList = ({styles, challenges, currentUser}) => (
-  <div> 
-   {challenges.map(function(challenge) {
-    return (
-      <div key={challenge.id}>
-        <Link to={currentUser === challenge.creatorId ? `/challenge/${challenge.id}/admin` : `/challenge/${challenge.id}`}>
-          <ChallengeListEntry styles={styles} challenge={challenge} />
-        </Link>
-      </div>
-    );
-   })}
-  </div>
-  );
+const ChallengeList = ({styles, challenges, currentUser, entities}) => {
+  challenges = challenges.map(id => entities.challenges[id]);
+  return ( 
+    <div> 
+     {challenges.map(function(challenge) {
+      return (
+        <div key={challenge.id}>
+          <Link to={currentUser === challenge.creatorId ? `/challenge/${challenge.id}/admin` : `/challenge/${challenge.id}`}>
+            <ChallengeListEntry styles={styles} challenge={challenge} />
+          </Link>
+        </div>
+      );
+     })}
+    </div>
+  )
+};
 
 ChallengeList.propTypes = {
-  challenges: React.PropTypes.array.isRequired
+  challenges: React.PropTypes.array.isRequired,
+  entities: React.PropTypes.object.isRequired
 };
 
 export default ChallengeList;
