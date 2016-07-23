9741fc3e5a8b3df7c8d0b8d614d944be6721676c
Frances Yang <franyang99@gamil.com>
2016-07-22 17:51:53 -0700

    Remove actions and reducers that we won't be using in MVP

--- client/src/actions/fetchChallenges.js
+++ client/src/actions/fetchChallenges.js
@@ -1,10 +1,3 @@
-//import fetch from 'isomorphic-fetch'
-export function requestChallenges() {
-  return {
-    type: 'REQUEST_CHALLENGE_LIST',
-  }
-}
-
 export function receiveChallenges(challenges) {
   return {
     type: 'RECEIVE_CHALLENGE_LIST',
@@ -24,26 +17,5 @@ export function fetchChallenges() {
         return res.json();
        })
       .then(json => dispatch(receiveChallenges(json)));
-  }
-}
-
-function shouldFetchChallenges(state) {
-  const challengeList = state.entities.challengeList;
-  if (!challengeList) {
-    return true;
-  } else if (challengeList.isFetching) {
-    return false;
-  } else {
-    return challengeList.didInvalidate;
-  }
-}
-
-export function fetchChallengesIfNeeded() {
-  return (dispatch, getState) => {
-    if (shouldFetchChallenges(getState())) {
-      return dispatch(fetchChallenges());
-    } else {
-      return Promise.resolve();
-    }
   }
 }
