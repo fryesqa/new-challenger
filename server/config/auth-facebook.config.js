47c1d6f7e638508563cb49ecaab15cc596b6605a
1kindjune <ejw322@nyu.edu>
2016-07-24 15:54:43 -0700

    Change facebook config name and gitignore

---
+++ server/config/auth-facebook.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  facebookAuth : {
+    'clientID' : '#############',
+    'clientSecret' : '#############',
+    'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+  }
+}
+
+//plugin information and rename file to auth-facebook.js
