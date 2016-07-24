8eb64be8bbb8605edc63a31826b60015c11c8a06
Frances <franyang99@gmail.com>
2016-07-24 15:23:25 -0700

    Edit props passed to children

--- client/src/components/AdminChallenge.jsx
+++ client/src/components/AdminChallenge.jsx
@@ -22,9 +22,9 @@ const imageStyle = {
 class AdminChallenge extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    console.log('adminchallenge', props);
   }
-
+ 
   handleClick(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -34,30 +34,31 @@ class AdminChallenge extends React.Component {
   //Instead of lines 22-25 insert a ChallengeListEntry with the challenge data as props
   //Figure out styling later
   render() {
-
+    const id = this.props.params.challengeId;
+    const challenge = this.props.entities.challenges[id];
     return (
       <Card style={adminChallengeStyle}>
         <CardHeader
-          title={props.challenge.username}
+          title={challenge.username}
           subtitle=""
           avatar="user-profile-image"
         />
         <CardMedia style={imageStyle}>
-          <img src={props.challenge.url} />
+          <img src={challenge.url} />
         </CardMedia>
-        <CardTitle title={props.challenge.name} subtitle={props.challenge.category} />
+        <CardTitle title={challenge.name} subtitle={challenge.category} />
         <CardText>
-          <div>{props.challenge.description}</div> 
+          <div>{challenge.description}</div> 
           <br />
-          <div>{props.challenge.successes} out of {props.challenge.challengers} challengers have completed this challenge! </div>
+          <div>{challenge.successes} out of {challenge.challengers} challengers have completed this challenge! </div>
           <br />
-          <div>Current challengers: {props.challenge.currentChallengers}</div>
-          <div>Completed: {props.challenge.successNames}</div>
+          <div>Current challengers: {challenge.currentChallengers}</div>
+          <div>Completed: {challenge.successNames}</div>
           <br />
-          <div>Start: {props.challenge.createdAt} </div>
-          <div>End: {props.challenge.endTime} </div>
+          <div>Start: {challenge.createdAt} </div>
+          <div>End: {challenge.endTime} </div>
 
-        <PendingApprovalList challengers={dummyData.adminChallenge.challengers} handleClick={this.handleClick} />
+        <PendingApprovalList players={this.props.playersOfUserChallenges[id]} handleClick={this.props.adminClick} entities={this.props.entities} />
 
         </CardText>
         <CardActions>
