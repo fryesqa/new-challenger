5639763c15f4986c359385b40372395cc1640948
Frances <franyang99@gmail.com>
2016-07-23 22:07:50 -0700

    Fix link in navigation

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => (
   <nav className="blue">
     <Link to="/"><span className="brand-logo">New Challenger</span></Link>
     <Link to="/login"><span className="right linkStyle">Login</span></Link>
-    <Link to="/challenge/create"><span className="right linkStyle">Create Challenge</span></Link>
+    <Link to="/challenges/create"><span className="right linkStyle">Create Challenge</span></Link>
   </nav>
 );
 
