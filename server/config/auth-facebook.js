fc53c002418e0e8831a9d1f1fc4862a63a65be58
Keith Wong <keith.wong3672@gmail.com>
2016-07-24 10:26:48 -0700

    Made sure info not shown

--- server/config/auth-facebook.js
+++ server/config/auth-facebook.js
@@ -1,7 +1,7 @@
 module.exports = {
   facebookAuth : {
-    'clientID' : '1692982380951867',
-    'clientSecret' : 'ee2ba4fabf81c8f850ffd23084127dfc',
+    'clientID' : '#############',
+    'clientSecret' : '#############',
     'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
   }
 }
