432146142aec39327cdac560db67bfee6d4f67bc
Frances <franyang99@gmail.com>
2016-07-23 22:09:52 -0700

    Fix the props passed to components for user profile

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -2,28 +2,39 @@ import React from 'react';
 import ChallengeList from './ChallengeList.jsx';
 import dummyData from './dummyData';
 
-const UserProfile = () => (
-  <div>
+const UserProfile = ({entities, currentUser}) => {
+  const user = entities.users[currentUser];
+  const idToChallenge = function(ids) {
+    return ids.map(id => entities.challenges[id]);
+  };  
+  return (
     <div>
-      <img src={this.state.user.imageUrl} alt=""></img>
-      <span>
-        <div>{this.state.user.username}</div>
-        <div>{this.state.user.email}</div>
-      </span>
+      <div>
+        <img src={user.imageUrl} alt=""></img>
+        <span>
+          <div>{user.username}</div>
+          <div>{user.email}</div>
+        </span>
+      </div>
+      <div>
+        <h3>My Challenges</h3>
+        <ChallengeList challenges={idToChallenge(user.challengesCreated)} />
+      </div>
+      <div>
+        <h4>Current Challenges</h4>
+        <ChallengeList challenges={idToChallenge(user.challengesTaken)} />
+      </div>
+      <div>
+        <h4>Completed Challenges</h4>
+        <ChallengeList challenges={idToChallenge(user.challengesCompleted)} />
+      </div>
     </div>
-    <div>
-      <h3>My Challenges</h3>
-      <ChallengeList challenges={this.state.user.challengesCreated} />
-    </div>
-    <div>
-      <h4>Current Challenges</h4>
-      <ChallengeList challenges={this.state.user.challengesTaken} />
-    </div>
-    <div>
-      <h4>Completed Challenges</h4>
-      <ChallengeList challenges={this.state.user.challengesCompleted} />
-    </div>
-  </div>
-);
+  );
+};
+
+UserProfile.propTypes = {
+  entities: React.PropTypes.object.isRequired,
+  currentUser: React.PropTypes.number.isRequired 
+};
 
 export default UserProfile;
