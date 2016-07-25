1e5bbda90f8b6505b7896cc5b7a0c378f218448f
Keith Wong <keith.wong3672@gmail.com>
2016-07-24 22:05:02 -0700

    Delete config file, this file not be be saved

--- server/config/auth-facebook.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  facebookAuth : {
-    'clientID' : '#############',
-    'clientSecret' : '#############',
-    'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
-  }
-}
