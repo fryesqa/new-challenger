a18056fef127679467536a6aaa6496ac02c4e21e
Frances <franyang99@gmail.com>
2016-07-25 12:32:18 -0700

    Add actions for players

--- client/src/actions/appActions.js
+++ client/src/actions/appActions.js
@@ -5,5 +5,6 @@ import { fetchChallenges } from './fetchChallenges';
 import { fetchUsers } from './fetchUsers';
 import { loginUser } from './loginUser';
 import { adminClick } from './adminClick';
+import { addPlayer } from './addPlayer';
 
-export { fetchChallenges, fetchUsers, loginUser };
+export { fetchChallenges, fetchUsers, adminClick, addPlayer, loginUser };
