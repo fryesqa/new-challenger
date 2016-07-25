c324469a21414eb276a60abc7f1a861fecfb41ce
Keith Wong <keith.wong3672@gmail.com>
2016-07-25 07:28:33 -0700

    Replace config/auth-facebook.js file with additional config info

---
+++ server/config/configExample.js
@@ -0,0 +1,11 @@
+module.exports = {
+  facebookAuth : {
+    'clientID' : '#############',
+    'clientSecret' : '#############',
+    'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+  },
+  herokuPostgresAuth: '#########################'
+
+}
+
+//plugin information and rename file to config.js
