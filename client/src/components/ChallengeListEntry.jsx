8e1a531b922dceb9a2e4f83d43d4dec89f013b56
1kindjune <ejw322@nyu.edu>
2016-07-25 20:13:29 -0700

    Clean up code in challenge list entry

--- client/src/components/ChallengeListEntry.jsx
+++ client/src/components/ChallengeListEntry.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import { Card, CardActions, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
-import classNames from 'classnames';
 
 
 const buttonStyle = {
   margin: 12,
   float: 'right',
-  color:'white'
+  color: 'white'
 };
 
 const cardStyle = {
@@ -19,23 +17,23 @@ const cardStyle = {
   overflow: 'hidden',
   float: 'left',
   margin: '1%'
-}
+};
 
 const titleStyle = {
-  height: '50px', 
-  overflow:'hidden'
-}
+  height: '50px',
+  overflow: 'hidden'
+};
 
 const textStyle = {
   height: '100px',
   overflow: 'hidden'
-}
+};
 
 const imageStyle = {
   width: '100%',
   height: '200px',
   overflow: 'hidden'
-}
+};
 
 
 // Needs more styling depending on fields
@@ -47,33 +45,26 @@ class ChallengeListEntry extends React.Component {
   }
 
   handleSignUp() {
-    
-    if(!this.props.currentUser) {
+    if (!this.props.currentUser) {
       alert('Please login to sign up for this challenge :)');
     } else {
-      console.log('CURRENT USER', this.props.currentUser);
       console.log('currentuser', this.props.currentUser, 'challengeid', this.props.challenge.id);
       this.props.signUpChallenge(this.props.currentUser, this.props.challenge.id);
     }
   }
-  
+
   handleClick(e) {
-    console.log('props', this.props);
     const id = this.props.challenge.id;
     this.props.challenge.currentChallengers.forEach(playerId => { this.props.addPlayer(id, playerId); });
     const loc = this.props.challenge.userId === this.props.currentUser.id ? `/challenges/${id}/admin` : `/challenges/${id}`;
     this.context.router.push(loc);
   }
 
-        // <CardHeader
-        //   title={this.props.challenge.username}
-        //   subtitle=""
-        // />
   render() {
     return (
       <Card style={cardStyle} >
         <CardMedia style={imageStyle} onTouchTap={this.handleClick}>
-          <img style={{height: '200px', objectFit: 'contain'}} src={this.props.challenge.url} />
+          <img style={{ height: '200px', objectFit: 'contain' }} src={this.props.challenge.url} />
         </CardMedia>
         <CardTitle style={titleStyle} title={this.props.challenge.name} subtitle={this.props.challenge.category} />
         <CardText style={textStyle}>
@@ -87,8 +78,7 @@ class ChallengeListEntry extends React.Component {
       </Card>
     );
   }
-
-};
+}
 
 ChallengeListEntry.propTypes = {
   challenge: React.PropTypes.object.isRequired
@@ -96,6 +86,6 @@ ChallengeListEntry.propTypes = {
 
 ChallengeListEntry.contextTypes = {
   router: React.PropTypes.object.isRequired
-}
+};
 
 export default ChallengeListEntry;
