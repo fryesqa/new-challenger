4b7df3c525bd7cbd4be4bc61fd572cc0cdf86fb6
1kindjune <ejw322@nyu.edu>
2016-07-25 20:05:15 -0700

    Clean up comments in main

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ChallengeList from './ChallengeList.jsx';
-import Navigation from './Navigation.jsx';
-import classNames from 'classnames';
 
 const Main = ({ challengeList, entities, currentUser, addPlayer, history, signUpChallenge }) => ( 
   <div>
@@ -18,22 +16,7 @@ const Main = ({ challengeList, entities, currentUser, addPlayer, history, signUp
       </div>
     </div>
     <ChallengeList challenges={challengeList.items} entities={entities} currentUser={currentUser} addPlayer={addPlayer} signUpChallenge={signUpChallenge} history={history} />
-
   </div>
-)
+);
 
 export default Main;
-
-// const exampleStyle = {
-//   'padding':'20px',
-//   'fontFamily':'futura',
-//   'backgroundColor': 'pink',
-//   'width': '200px'
-// };
-    
-    // <div style={exampleStyle}>
-    //   Example of Redux
-    //   {/*bind so that on page load the props.increment wont run*/}
-    //   <button onClick={props.increment.bind(null, props.index)}>Increment Using Actions</button>
-    //   <div>Index: {props.index}</div>
-    // </div>  
